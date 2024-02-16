048e2b60df14f08dcb82221cfd21b7cb476c3dd8
roopa-sgb <roopa_sgb@yahoo.co.in>
2024-02-16 21:52:41 +0530

    new learnings

--- App.js
+++ App.js
@@ -1,25 +1,19 @@
-
+import React from "react";
+import  ReactDOM from "react-dom/client";
        
 
-/**
- * <div id="parent">
- *    <div id="child1">
- *        <h1>I am h1 child</h1>
- *        <h2>I am h2 child</h2>
- *    </div>
- *    <div id="child2">
- *        <h1>I am h1 child</h1>
- *        <h2>I am h2 child</h2>
- *    </div>
- * </div>
- */        
 
- const heading = React.createElement("div",{id: "parent"},
-                [ React.createElement("div",{id:"child1"},
-                 [React.createElement("h1",{},"I am h1 child"), React.createElement("h2",{},"I am h2 child")]),
-                 React.createElement("div",{id:"child1"},
-                 [React.createElement("h1",{},"I am h1 child"), React.createElement("h2",{},"I am h2 child")])]);
 
-                 const root = ReactDOM.createRoot(document.getElementById("root"));
-                 root.render(heading);
-                 console.log(heading);                
+ const Heading = function () {
+    return <h1 className="heading">Namaste React from JSX 🏆</h1>
+ } 
+ const Heading2 = () => (
+     <>
+    <Heading />
+    <h2 id="heading2">Heading2 from functional component 🧘‍♀️</h2>
+    </>
+ );
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Heading2 />);
+               
